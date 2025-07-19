a7a953b1fce47700615dc36c807f756b27e368a3
ayutac <fly.high.android@gmail.com>
2025-07-19 19:28:13 +0200

    Made JMForgeLootModifiers an interface.

--- forge/src/main/java/com/jcraft_eoe/jjbamusic/forge/loot/JMForgeLootModifiers.java
+++ forge/src/main/java/com/jcraft_eoe/jjbamusic/forge/loot/JMForgeLootModifiers.java
@@ -8,14 +8,14 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
-public class JMForgeLootModifiers {
-    public static final DeferredRegister<Codec<? extends IGlobalLootModifier>> LOOT_MODIFIER_SERIALIZERS =
+public interface JMForgeLootModifiers {
+    DeferredRegister<Codec<? extends IGlobalLootModifier>> LOOT_MODIFIER_SERIALIZERS =
             DeferredRegister.create(ForgeRegistries.Keys.GLOBAL_LOOT_MODIFIER_SERIALIZERS, JjbaMusic.MOD_ID);
 
-    public static final RegistryObject<Codec<? extends IGlobalLootModifier>> FORGE_LOOT_ADAPTER =
+    RegistryObject<Codec<? extends IGlobalLootModifier>> FORGE_LOOT_ADAPTER =
             LOOT_MODIFIER_SERIALIZERS.register("loot_adapter", JMForgeLootAdapter.CODEC);
 
-    public static void register(final IEventBus eventBus) {
+    static void register(final IEventBus eventBus) {
         LOOT_MODIFIER_SERIALIZERS.register(eventBus);
     }
 }
