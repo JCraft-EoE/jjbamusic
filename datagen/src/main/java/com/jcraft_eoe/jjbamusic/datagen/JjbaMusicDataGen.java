c57039e559e4289574896e87240c4adb8be48646
PlanetTeamSpeak <PlanetTeamSpeakk@users.noreply.github.com>
2025-07-19 15:28:28 +0200

    Create datagen subproject

---
+++ datagen/src/main/java/com/jcraft_eoe/jjbamusic/datagen/JjbaMusicDataGen.java
@@ -0,0 +1,12 @@
+package com.jcraft_eoe.jjbamusic.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class JjbaMusicDataGen implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator generator) {
+        final FabricDataGenerator.Pack pack = generator.createPack();
+        // TODO do some data generation
+    }
+}
